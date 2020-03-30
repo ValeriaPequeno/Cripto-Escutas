1be4414e9fa8ed5928c8d29e3bc7395e8603deb1
mrhenry549 <henrique.prego@outlook.pt>
2020-03-30 14:49:53 +0100

    Criação do ficheiro de encriptações

---
+++ app/src/main/java/com/example/bhjencryption/Encriptacoes.java
@@ -0,0 +1,25 @@
+package com.example.bhjencryption;
+
+public class Encriptacoes {
+
+    public static String cesarDecode(String enc, int offset) {
+        return cesarEncode(enc, 26-offset);
+    }
+
+    public static String cesarEncode(String enc, int offset) {
+        offset = offset % 26 + 26;
+        StringBuilder encoded = new StringBuilder();
+        for (char i : enc.toCharArray()) {
+            if (Character.isLetter(i)) {
+                if (Character.isUpperCase(i)) {
+                    encoded.append((char) ('A' + (i - 'A' + offset) % 26 ));
+                } else {
+                    encoded.append((char) ('a' + (i - 'a' + offset) % 26 ));
+                }
+            } else {
+                encoded.append(i);
+            }
+        }
+        return encoded.toString();
+    }
+}
